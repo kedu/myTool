13de8df57794afb73dae0fcb2e760bed6c76d03a
likangbin <1218773641@qq.com>
2017-02-21 22:45:23 +0800

    初始化

---
+++ myTool/myTool/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myTool
+//
+//  Created by Apple on 17/2/21.
+//  Copyright © 2017年 lkb-求工作qq:1218773641. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
